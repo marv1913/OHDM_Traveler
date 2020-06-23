48a18e3212465e60c3f7d1c430240b516ec2fbbf
Marvin Rausch <marvin.rausch@live.de>
2020-06-23 12:38:08 +0200

    updated build.gradle to generate fat jar's

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -3,6 +3,7 @@ package util;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 import org.json.simple.parser.ParseException;
 import rest.RequestDAO;
 import rest.SearchRequestDAO;
